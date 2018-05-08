8b1b5d7b0cf18abadd07bcce8a04e66f7a4a0376
ryota-sakamoto <sakamo.ryota+github@gmail.com>
2018-05-09 00:54:05 +0900

    close #1

--- init.sh
+++ init.sh
@@ -31,4 +31,10 @@ curl -L https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable -o /
 dpkg -i /tmp/vscode.deb
 rm -rf /tmp/vscode.deb
 
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable test edge"
+sudo apt update
+sudo apt install docker-ce -y
+
 sudo shutdown -r now
